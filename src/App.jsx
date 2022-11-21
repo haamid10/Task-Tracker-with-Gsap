3dfd31ce6c18859399344477eed2c596ab31b349
haamid10 <apdixamiid919@gmail.com>
2022-11-21 14:03:40 +0300

    madpushed the in the useState in app.jsx also declered as props to excess data in tasks section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
+import { useState } from "react"
 const App = () => {
+  const [tasks,setTasks]=useState( [ {
+    id: 1,
+    text: "Doctors Appointment",
+    day: "Feb 5th at 2:28pm",
+     reminder: true
+},
+{
+    id: 2,
+    text: "Meeting at the School",
+    day: "Feb 6th at 3:28pm",
+     reminder: true
+},
+{
+    id: 3,
+    text: "Food shopping",
+    day: "Feb 7th at 9:28pm",
+    reminder: false
+}
+])
   return (
     <div className="container">
       <Header />
-      <Tasks/>
+      <Tasks  tasks={tasks}/>
       <h1  ></h1>
     </div>
   )
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,30 +1,9 @@
-const tasks= [
 
-  {
-      id: 1,
-      text: "Doctors Appointment",
-      day: "Feb 5th at 2:28pm",
-       reminder: true
-  },
-  {
-      id: 2,
-      text: "Meeting at the School",
-      day: "Feb 6th at 3:28pm",
-       reminder: true
-  },
-  {
-      id: 3,
-      text: "Food shopping",
-      day: "Feb 7th at 9:28pm",
-      reminder: false
-  }
 
-]
-
-const Tasks = () => {
+const Tasks = (props) => {
   return (
     <div className="task">
-      {tasks.map((task)=><h2>{task.text}</h2>)}
+      {props.tasks.map((task)=><h2>{task.text}</h2>)}
     </div>
   )
 }
