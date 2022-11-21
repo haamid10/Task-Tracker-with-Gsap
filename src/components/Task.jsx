97ea9312aa6bedcdc4b0db9c28081e9cbf46a316
haamid10 <apdixamiid919@gmail.com>
2022-11-21 20:36:42 +0300

    added to to react Icons

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,8 +1,11 @@
 
+import {FaTimes} from "react-icons/fa"
 const Task = ({task}) => {
   return (
     <div className="task">
-      <h2>{task.text}</h2>
+      <h2>{task.text} 
+       <FaTimes/></h2>
+      
       <p>{task.day}</p>
       <h3>{task.reminder}</h3>
       
