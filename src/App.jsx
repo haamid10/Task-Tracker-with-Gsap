3e8e816579687eedc0dcdd294876cfe27d370b6e
haamid10 <apdixamiid919@gmail.com>
2022-11-26 13:13:18 +0300

    backend mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
-import { useState } from "react"
+import { useState,useEffect } from "react"
 import AddTask from "./components/AddTask"
 const App = () => {
-  const [tasks,setTasks]=useState( [ {
-    id: 1,
-    text: "Doctors Appointment",
-    day: "Feb 5th at 2:28pm",
-     reminder: true
-},
-{
-    id: 2,
-    text: "Meeting at the School",
-    day: "Feb 6th at 3:28pm",
-     reminder: true
-},
-{
-    id: 3,
-    text: "Food shopping",
-    day: "Feb 7th at 9:28pm",
-    reminder: false
-}
-])
+  const [tasks,setTasks]=useState([])
 const [showAdd , setShowAdd]=useState(false)
 const [showRes , setShoeRes]=useState(false)
+
+
 
 // add Task
 const addTask =(task)=>{
