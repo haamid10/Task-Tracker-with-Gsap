2487963aabbcc155f97fb1d1fb09ea2c5549b2c4
haamid10 <apdixamiid919@gmail.com>
2022-11-27 13:03:33 +0300

    connected back-end to the foem

--- src/App.jsx
+++ src/App.jsx
@@ -24,10 +24,14 @@ const fetchTasks = async () => {
 
 
 // add Task
-const addTask = (task)=>{
-  const id= Math.floor(Math.random()*1000)+1
-  const newTask= {id,...task}
-  setTasks([...tasks,newTask])
+const addTask = async (task)=>{
+  const res= await fetch('http://localhost:5000/tasks/',{
+    method:'POST',
+    headers: {"content-type":"application/json"},
+    body: JSON.stringify(task)
+  })
+  const data = await res.json()
+  setTasks([...tasks,data])
 }
 // delete 
 const deleteBtn =async (id) =>{
