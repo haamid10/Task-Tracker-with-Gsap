55f2f3007335b9397aa9d040cc292103e0a7798b
haamid10 <apdixamiid919@gmail.com>
2022-11-27 10:15:42 +0300

    fetching data from json.server

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,20 @@ const App = () => {
 const [showAdd , setShowAdd]=useState(false)
 const [showRes , setShoeRes]=useState(false)
 
+useEffect(()=>{
+  const getTasks = async ()=>{
+    const tasksFromServer = await fetchTasks()
+    setTasks(tasksFromServer)
+  }
+  getTasks()
+ 
+},[])
+const fetchTasks = async () => {
+  const res= await fetch('http://localhost:5000/tasks/')
+  const data= await res.json()
+  console.log(data)
+  return data
+}
 
 
 // add Task
